8f3aa50b279a6f59d0a80b6d24d84bbdd8943927
Bryan Marfito <bjmarfito@yahoo.com>
2023-11-23 22:27:55 +0800

    Update ar2fc.m

--- slipBERImbin/ar2fc.m
+++ slipBERImbin/ar2fc.m
@@ -1,7 +1,7 @@
 % Function to transform Aki-Richards to fault centered coordinates
 function[latc, lonc] = ar2fc(lata2r, lona2r, faultStrike, faultLength)
 
-    arcLen = -1 .* km2deg(0.5*faultLength);
+    arcLen = km2deg(0.5*faultLength);
     [latc, lonc] = reckon(lata2r, lona2r, arcLen, faultStrike);
 
 end
