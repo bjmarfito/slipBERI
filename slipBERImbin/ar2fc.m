7f3c07623c11874870614f88dbec08dbf976026d
Bryan Marfito <bjmarfito@yahoo.com>
2023-11-23 22:11:23 +0800

    Update ar2fc.m

--- slipBERImbin/ar2fc.m
+++ slipBERImbin/ar2fc.m
@@ -3,7 +3,7 @@ function[lonc, latc] = ar2fc(lona2r, lata2r, faultStrike, faultLength)
 
     arcLen = km2deg(0.5*faultLength);
     rotateCenterToAR = faultStrike + 180;
-    [lonc, latc] = reckon(lata2r, lona2r, arcLen, rotateCenterToAR);
+    [latc, lonc] = reckon(lata2r, lona2r, arcLen, rotateCenterToAR);
     
 end
 
